eef15d787361fbd3abe04426c3af3e7628a5249d
Himanshu <99754294+Kazuto335@users.noreply.github.com>
2024-06-11 09:49:18 +0530

    Create main.py

---
+++ EmailSMTP/main.py
@@ -0,0 +1,34 @@
+import random
+import smtplib
+from datetime import *
+
+if datetime.weekday == 0:
+    with open(file='quotes.txt') as file:
+        # file = file.readlines()
+        # file = random.choice(file)
+        # message = file
+        # print(message)
+
+        file = file.read().split('\n')
+        index = random.randint(0, len(file))
+        message = file[index]
+
+    message = message.split('"')
+    # print(message[2])
+    # print(message)
+
+    SENDER = 'ranahimanshu335@gmail.com'
+    PASSWORD = 'qzjhnbypiwamgtua'
+    RECEIVER = 'kalatomu@gmail.com'
+
+    conn = smtplib.SMTP("smtp.gmail.com")
+    conn.starttls()
+    conn.login(SENDER, PASSWORD)
+    conn.sendmail(from_addr=SENDER,
+                  to_addrs=RECEIVER,
+                  msg=f'{message[1]}\n{message[2]}')
+    conn.close()
+
+else:
+    weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    print(f"Today's {weekday[datetime.now().weekday()]}, you have to wait for Monday.")
