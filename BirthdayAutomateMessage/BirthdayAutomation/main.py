6b869d4b8eb449c0e8023f02e8854df78bd4aef9
Himanshu <99754294+Kazuto335@users.noreply.github.com>
2024-06-11 15:46:10 +0530

    Add files via upload

---
+++ BirthdayAutomateMessage/BirthdayAutomation/main.py
@@ -0,0 +1,48 @@
+from tkinter import messagebox
+from pandas import *
+from datetime import *
+from random import *
+from smtplib import *
+
+SENDER = 'ranahimanshu335@gmail.com'
+PASSWORD = 'qzjhnbypiwamgtua'
+LIST_OF_LETTERS = ('letter_1.txt', 'letter_2.txt', 'letter_3.txt')
+
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+df = read_csv('birthdays.csv')
+# print(df.shape[0])
+# print(len(df.index))
+for index in range(0, len(df.index)):
+    receiver = df.at[index, 'email']
+    # print(type(RECEIVER))
+    MONTH: object = df.at[index, 'month']
+    DAY: object = df.at[index, 'day']
+    NAME = df.at[index, 'name']
+
+    # print(f"{DAY}/{MONTH}")
+    # 2. Check if today matches a birthday in the birthdays.csv
+    current_day = datetime.now().day
+    current_month = datetime.now().month
+
+    if current_day == DAY and current_month == MONTH:
+
+        # 3. If step 2 is true, pick a random letter from letter templates and
+        # replace the [NAME] with the person's actual name from birthdays.csv
+        letter_template = choice(LIST_OF_LETTERS)
+        OLD_NAME = '[NAME]'
+        NEW_NAME = NAME
+        with open(file=f'letter_templates/{letter_template}') as letter:
+            letter = letter.read()
+            LETTER = letter.replace(OLD_NAME, NEW_NAME)
+
+        # 4. Send the letter generated in step 3 to that person's email address.
+        with SMTP('smtp.gmail.com') as conn:
+            conn.starttls()
+            conn.login(SENDER, PASSWORD)
+            conn.sendmail(SENDER, receiver, msg=LETTER)
+            conn.close()
+            messagebox.showinfo('Done', f'Message Send to {NEW_NAME}')
+    else:
+        messagebox.showinfo("Nobody's BIRTHDAY", "Nobody's birthday today.")
---
+++ BirthdayAutomateMessage/BirthdayAutomation/test.py
@@ -0,0 +1,54 @@
+# with open(file='birthdays.csv') as birthday_list:
+#     print(birthday_list.read())
+
+from pandas import *
+df = read_csv('birthdays.csv')
+# print(df.shape[0])
+# print(len(df.index))
+
+# MONTH = 0
+# DAY = 0
+for index in range(0, len(df.index)):
+    MONTH = df.at[index, 'month']
+    DAY = df.at[index, 'day']
+    NAME = df.at[index, 'name']
+
+print(f"{DAY}/{MONTH}")
+
+from random import *
+from datetime import *
+
+list_of_files = ['letter_1.txt', 'letter_2.txt', 'letter_3.txt']
+
+current_day = datetime.now().day
+current_month = datetime.now().month
+
+if current_day == 11 or current_month == 6:
+    OLD_NAME = '[NAME]'
+    NEW_NAME = NAME
+    letter_template = choice(list_of_files)
+    with open(file=f'letter_templates/{letter_template}') as letter:
+        letter = letter.read()
+        LETTER = letter.replace(OLD_NAME, NEW_NAME)
+
+# print(LETTER)
+
+import smtplib
+
+SENDER = 'ranahimanshu335@gmail.com'
+PASSOWRD = 'qzjhnbypiwamgtua'
+RECEIVER = 'kalatomu@gmail.com'
+
+with smtplib.SMTP('smtp.gmail.com') as conn:
+    conn.starttls()
+    conn.login(SENDER,PASSOWRD)
+    conn.sendmail(SENDER,RECEIVER,msg=LETTER)
+    conn.close()
+
+
+
+
+
+
+
+
