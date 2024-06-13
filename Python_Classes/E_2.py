99596085643b5d7ff6e2a246d4706a7e6ceaee83
Himanshu <99754294+Kazuto335@users.noreply.github.com>
2024-06-13 23:33:29 +0530

    Create E_2.py

---
+++ Python_Classes/E_2.py
@@ -0,0 +1,18 @@
+import random
+
+# Don't change this code (Working of this code create list of 10 random numbers between 10-100)
+list_ = [random.randint(10,100) for value in range(0, 10)]
+# print(list_)
+# Remove the above Comment and print the list if you want to check the list data
+
+# ---- write your code below ----
+# TODO 1: Empty the List created above.
+
+
+# TODO 2: Input 5 random Elements between(0,10) in the list using loop don't use the above method i Use.
+# HINT : use random.randint(pass the range here) this will give you a random number every time between the range
+
+
+# TODO 3: Now Sort the List in accending Order (like: 0->1->2->3->4) .
+
+
