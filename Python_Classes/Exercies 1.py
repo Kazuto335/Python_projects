8bac6208c59c54e6133e9b6e87ff89bd1240c2b6
Himanshu <99754294+Kazuto335@users.noreply.github.com>
2024-06-13 23:07:14 +0530

    Create Exercies 1.py

---
+++ Python_Classes/Exercies 1.py
@@ -0,0 +1,10 @@
+# Don't Change Code Below
+list = []
+
+# Add values [2,4,6,8,10] in list
+# Hint: Use input() to take input
+# ---- Write your code ----
+
+
+# Now write a program to find the sum of all the elements in the list.
+
